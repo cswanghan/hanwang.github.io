8a3175e63b12f3bd983abae6db14dd5c13f46c34
wanghan.wh <wanghan.wh@alibaba-inc.com>
2016-04-08 16:25:46 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html>
+ <head> 
+  <meta charset="utf-8" /> 
+  <title>Chars Davy</title> 
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
+  <meta name="description" /> 
+  <meta property="og:type" content="website" /> 
+  <meta property="og:title" content="Chars Davy" /> 
+  <meta property="og:url" content="http://yoursite.com/index.html" /> 
+  <meta property="og:site_name" content="Chars Davy" /> 
+  <meta property="og:description" /> 
+  <meta name="twitter:card" content="summary" /> 
+  <meta name="twitter:title" content="Chars Davy" /> 
+  <meta name="twitter:description" /> 
+  <link rel="alternative" href="/atom.xml" title="Chars Davy" type="application/atom+xml" /> 
+  <link rel="icon" href="/favicon.png" /> 
+  <link rel="stylesheet" href="/css/style.css" type="text/css" /> 
+ </head> 
+ <body> 
+  <div id="container"> 
+   <div class="left-col"> 
+    <div class="overlay"></div> 
+    <div class="intrude-less"> 
+     <header id="header" class="inner"> 
+      <a href="/" class="profilepic"> <img lazy-src="/img/myAvatar.png" class="js-avatar" /> </a> 
+      <hgroup> 
+       <h1 class="header-author"><a href="/">Chars Davy</a></h1> 
+      </hgroup> 
+      <p class="header-subtitle">无所谓的态度，过着随遇而安的生活</p> 
+      <div class="switch-btn"> 
+       <div class="icon"> 
+        <div class="icon-ctn"> 
+         <div class="icon-wrap icon-house" data-idx="0"> 
+          <div class="birdhouse"></div> 
+          <div class="birdhouse_holes"></div> 
+         </div> 
+         <div class="icon-wrap icon-ribbon hide" data-idx="1"> 
+          <div class="ribbon"></div> 
+         </div> 
+         <div class="icon-wrap icon-link hide" data-idx="2"> 
+          <div class="loopback_l"></div> 
+          <div class="loopback_r"></div> 
+         </div> 
+         <div class="icon-wrap icon-me hide" data-idx="3"> 
+          <div class="user"></div> 
+          <div class="shoulder"></div> 
+         </div> 
+        </div> 
+       </div> 
+       <div class="tips-box hide"> 
+        <div class="tips-arrow"></div> 
+        <ul class="tips-inner"> 
+         <li>菜单</li> 
+         <li>标签</li> 
+         <li>友情链接</li> 
+         <li>关于我</li> 
+        </ul> 
+       </div> 
+      </div> 
+      <div class="switch-area"> 
+       <div class="switch-wrap"> 
+        <section class="switch-part switch-part1"> 
+         <nav class="header-menu"> 
+          <ul> 
+           <li><a href="/">主页</a></li> 
+           <li><a href="/archives">所有文章</a></li> 
+          </ul> 
+         </nav> 
+         <nav class="header-nav"> 
+          <div class="social"> 
+           <a class="github" target="_blank" href="https://github.com/charsdavy" title="github">github</a> 
+           <a class="zhihu" target="_blank" href="https://www.zhihu.com/people/chars" title="zhihu">zhihu</a> 
+           <a class="rss" target="_blank" href="/atom.xml" title="rss">rss</a> 
+          </div> 
+         </nav> 
+        </section> 
+        <section class="switch-part switch-part2"> 
+         <div class="widget tagcloud" id="js-tagcloud"> 
+          <a href="/tags/iOS/" style="font-size: 20px;">iOS</a> 
+          <a href="/tags/效率/" style="font-size: 10px;">效率</a> 
+          <a href="/tags/碎碎念/" style="font-size: 10px;">碎碎念</a> 
+         </div> 
+        </section> 
+        <section class="switch-part switch-part3"> 
+         <div id="js-friends"> 
+          <a target="_blank" class="main-nav-link switch-friends-link" href="http://exodia.net/">Exodia</a> 
+         </div> 
+        </section> 
+        <section class="switch-part switch-part4"> 
+         <div id="js-aboutme">
+          Chars@ZAKER。竞技体育纪录片爱好者，希望有生之年玩遍各种运动。
+         </div> 
+        </section> 
+       </div> 
+      </div> 
+     </header> 
+    </div> 
+   </div> 
+   <div class="mid-col"> 
+    <nav id="mobile-nav"> 
+     <div class="overlay"> 
+      <div class="slider-trigger"></div> 
+      <h1 class="header-author js-mobile-header hide">Chars Davy</h1> 
+     </div> 
+     <div class="intrude-less"> 
+      <header id="header" class="inner"> 
+       <div class="profilepic"> 
+        <img lazy-src="/img/myAvatar.png" class="js-avatar" /> 
+       </div> 
+       <hgroup> 
+        <h1 class="header-author">Chars Davy</h1> 
+       </hgroup> 
+       <p class="header-subtitle">无所谓的态度，过着随遇而安的生活</p> 
+       <nav class="header-menu"> 
+        <ul> 
+         <li><a href="/">主页</a></li> 
+         <li><a href="/archives">所有文章</a></li> 
+         <div class="clearfix"></div> 
+        </ul> 
+       </nav> 
+       <nav class="header-nav"> 
+        <div class="social"> 
+         <a class="github" target="_blank" href="https://github.com/charsdavy" title="github">github</a> 
+         <a class="zhihu" target="_blank" href="https://www.zhihu.com/people/chars" title="zhihu">zhihu</a> 
+         <a class="rss" target="_blank" href="/atom.xml" title="rss">rss</a> 
+        </div> 
+       </nav> 
+      </header> 
+     </div> 
+    </nav> 
+    <div class="body-wrap"> 
+     <article id="post-new" class="article article-type-post" itemscope="" itemprop="blogPost"> 
+      <div class="article-meta"> 
+       <a href="/2016/04/08/iOS-Xcode-Plugin/" class="article-date"> <time datetime="2016-04-08T01:53:03.000Z" itemprop="datePublished">2016-04-08</time> </a> 
+      </div> 
+      <div class="article-inner"> 
+       <input type="hidden" class="isFancy" /> 
+       <header class="article-header"> 
+        <h1 itemprop="name"> <a class="article-title" href="/2016/04/08/iOS-Xcode-Plugin/">Xcode插件开发教程指南</a> </h1> 
+       </header> 
+       <div class="article-entry" itemprop="articleBody"> 
+        <p>在平时开发过程中我们使用了很多的Xcode插件，虽然官方对于插件制作没有提供任何支持，但是加载三方的插件，默认还是被允许的。第三方的插件，存放在 ~/Library/Application Support/Developer/Shared/Xcode/Plug-ins文件夹中，后缀名必须是.xcplugin ，其实际上是一种bundle。所以我们创建一个插件工程，直接创建bundle工程即可。然后通过修改后缀名为.xcplugin，将其放到~/Library/Application Support/Developer/Shared/Xcode/Plug-ins目录中即可。</p> 
+        <p>本文主要介绍如何开发Xcode插件，并以实际开发案例做演示。</p> 
+       </div> 
+       <div class="article-info article-info-index"> 
+        <div class="article-tag tagcloud"> 
+         <ul class="article-tag-list">
+          <li class="article-tag-list-item"><a class="article-tag-list-link" href="/tags/iOS/">iOS</a></li>
+         </ul> 
+        </div> 
+        <div class="clearfix"></div> 
+       </div> 
+      </div> 
+     </article> 
+     <article id="post-iOS-inner-messaging" class="article article-type-post" itemscope="" itemprop="blogPost"> 
+      <div class="article-meta"> 
+       <a href="/2016/04/05/iOS-inner-messaging/" class="article-date"> <time datetime="2016-04-05T06:05:22.000Z" itemprop="datePublished">2016-04-05</time> </a> 
+      </div> 
+      <div class="article-inner"> 
+       <input type="hidden" class="isFancy" /> 
+       <header class="article-header"> 
+        <h1 itemprop="name"> <a class="article-title" href="/2016/04/05/iOS-inner-messaging/">iOS页面内消息通信机制</a> </h1> 
+       </header> 
+       <div class="article-entry" itemprop="articleBody"> 
+        <p>iOS开发中，经常需要在各个模块或者组件间进行消息通信，主要分为两种类别，一种是页面之间的（跨越不同的ViewController），一种是页面内部的（在同一个ViewController），两种类别由于场景不同采用的技术方案也不同，本文主要探讨的是页面内部的消息通知机制。</p> 
+       </div> 
+       <div class="article-info article-info-index"> 
+        <div class="article-tag tagcloud"> 
+         <ul class="article-tag-list">
+          <li class="article-tag-list-item"><a class="article-tag-list-link" href="/tags/iOS/">iOS</a></li>
+         </ul> 
+        </div> 
+        <div class="clearfix"></div> 
+       </div> 
+      </div> 
+     </article> 
+     <article id="post-iOS-memory-leak" class="article article-type-post" itemscope="" itemprop="blogPost"> 
+      <div class="article-meta"> 
+       <a href="/2016/04/04/iOS-memory-leak/" class="article-date"> <time datetime="2016-04-04T02:39:57.000Z" itemprop="datePublished">2016-04-04</time> </a> 
+      </div> 
+      <div class="article-inner"> 
+       <input type="hidden" class="isFancy" /> 
+       <header class="article-header"> 
+        <h1 itemprop="name"> <a class="article-title" href="/2016/04/04/iOS-memory-leak/">谈谈iOS内存泄露</a> </h1> 
+       </header> 
+       <div class="article-entry" itemprop="articleBody"> 
+        <p>内存泄露问题是每个app都必须关注的问题，关系到线上的稳定性和性能。内存泄露会导致：</p> 
+        <ul> 
+         <li>由于内存资源不足发生难以排查的线上bug</li> 
+         <li>由于被泄露对象的存在导致的一些业务bug，比如泄露的对象仍在接受全局通知</li> 
+         <li>性能问题，导致用户使用app时越来越卡<br />内存泄露发现分成静态扫描和运行时检测，本文主要讲运行时检测。</li> 
+        </ul> 
+       </div> 
+       <div class="article-info article-info-index"> 
+        <div class="article-tag tagcloud"> 
+         <ul class="article-tag-list">
+          <li class="article-tag-list-item"><a class="article-tag-list-link" href="/tags/iOS/">iOS</a></li>
+         </ul> 
+        </div> 
+        <div class="clearfix"></div> 
+       </div> 
+      </div> 
+     </article> 
+     <article id="post-google-tips" class="article article-type-post" itemscope="" itemprop="blogPost"> 
+      <div class="article-meta"> 
+       <a href="/2016/04/03/google-tips/" class="article-date"> <time datetime="2016-04-03T08:33:41.000Z" itemprop="datePublished">2016-04-03</time> </a> 
+      </div> 
+      <div class="article-inner"> 
+       <input type="hidden" class="isFancy" /> 
+       <header class="article-header"> 
+        <h1 itemprop="name"> <a class="article-title" href="/2016/04/03/google-tips/">Google搜索实用小技巧</a> </h1> 
+       </header> 
+       <div class="article-entry" itemprop="articleBody"> 
+        <p>随着互联网的爆发，知识库日渐丰富，信息量日益海量，那么，我们如何在这信息海洋中寻找我们自己所需要的信息呢？这无疑是个我们时时面临的问题，怎样才能迅速准确的搜索到我们需要的信息呢？本文主要分享几个Google搜索的实用小技巧。</p> 
+       </div> 
+       <div class="article-info article-info-index"> 
+        <div class="article-tag tagcloud"> 
+         <ul class="article-tag-list">
+          <li class="article-tag-list-item"><a class="article-tag-list-link" href="/tags/效率/">效率</a></li>
+         </ul> 
+        </div> 
+        <div class="clearfix"></div> 
+       </div> 
+      </div> 
+     </article> 
+     <article id="post-new" class="article article-type-post" itemscope="" itemprop="blogPost"> 
+      <div class="article-meta"> 
+       <a href="/2016/04/02/new/" class="article-date"> <time datetime="2016-04-02T15:53:03.000Z" itemprop="datePublished">2016-04-02</time> </a> 
+      </div> 
+      <div class="article-inner"> 
+       <input type="hidden" class="isFancy" /> 
+       <header class="article-header"> 
+        <h1 itemprop="name"> <a class="article-title" href="/2016/04/02/new/">接客啦</a> </h1> 
+       </header> 
+       <div class="article-entry" itemprop="articleBody"> 
+        <p>接客啦，看着网上很多人都有自己的博客专栏神马的。现在自己也来弄个自己的技术博客。</p> 
+       </div> 
+       <div class="article-info article-info-index"> 
+        <div class="article-tag tagcloud"> 
+         <ul class="article-tag-list">
+          <li class="article-tag-list-item"><a class="article-tag-list-link" href="/tags/碎碎念/">碎碎念</a></li>
+         </ul> 
+        </div> 
+        <div class="clearfix"></div> 
+       </div> 
+      </div> 
+     </article> 
+    </div> 
+    <footer id="footer"> 
+     <div class="outer"> 
+      <div id="footer-info"> 
+       <div class="footer-left">
+         &copy; 2016 Chars 
+       </div> 
+      </div> 
+     </div> 
+    </footer> 
+   </div> 
+   <link rel="stylesheet" href="/fancybox/jquery.fancybox.css" type="text/css" /> 
+   <script>
+	var yiliaConfig = {
+		fancybox: true,
+		mathjax: true,
+		animate: true,
+		isHome: true,
+		isPost: false,
+		isArchive: false,
+		isTag: false,
+		isCategory: false,
+		open_in_new: false
+	}
+</script> 
+   <!--<script src="http://7.url.cn/edu/jslib/comb/require-2.1.6,jquery-1.9.1.min.js" type="text/javascript"></script>--> 
+   <script src="/js/require-2.1.6,jquery-1.9.1.min.js" type="text/javascript"></script> 
+   <script src="/js/main.js" type="text/javascript"></script> 
+   <script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+    tex2jax: {
+        inlineMath: [ ['$','$'], ["\\(","\\)"]  ],
+        processEscapes: true,
+        skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
+    }
+});
+
+MathJax.Hub.Queue(function() {
+    var all = MathJax.Hub.getAllJax(), i;
+    for(i=0; i < all.length; i += 1) {
+        all[i].SourceElement().parentNode.className += ' has-jax';                 
+    }       
+});
+</script> 
+   <!--<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">--> 
+   <script type="text/javascript" src="/js/MathJax.js">
+</script> 
+  </div>  
+ </body>
+</html>
